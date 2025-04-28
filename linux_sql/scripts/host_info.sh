b77399b5ce3f439bf63318237c6c0e2ff7a086a8
Sara Zhiliu He <sarazhiliuhe@gmail.com>
2025-04-28 04:00:01 +0000

    feat: complete host_usage.sh with testings passed

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -27,7 +27,7 @@ timestamp=$(date '+%F %T')
 
 # construct the INSERT statement
 insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, \"timestamp\", total_mem)
-            VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$timestamp', '$total_mem')"
+            VALUES('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, $l2_cache, '$timestamp', $total_mem)"
 
 # Export password
 export PGPASSWORD=$psql_password
